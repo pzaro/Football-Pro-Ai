474373c51fa76a36ff9c0a6fbba802a29cdce1ae
pzaro <pzaro2010@gmail.com>
2026-04-13 12:05:11 +0300

    Add files via upload

--- leagues.js
+++ leagues.js
@@ -1,4 +1,5 @@
-// leagues.js - GLOBAL CONFIG & DATA
+// leagues.js
+
 const LEAGUES_DATA = [
   {id:2,  name:"Champions League"},     {id:3,  name:"Europa League"},
   {id:848,name:"Conference League"},    {id:39, name:"Premier League (EN)"},
@@ -19,9 +20,10 @@ const LEAGUES_DATA = [
 ];
 
 const LEAGUE_IDS = LEAGUES_DATA.map(l => l.id);
-const TRAP_LEAGUES = new Set([40,41,136,141,79,62,6,10,66,357, 239, 280, 268]);
-const TIGHT_LEAGUES = new Set([61,94,197,135,140,39,2,3,848, 128]);
-const GOLD_LEAGUES = new Set([78,262,88,253, 71, 113, 103]);
+
+const TRAP_LEAGUES = new Set([40,41,136,141,79,62,6,10,66,357, 239, 280, 268]); 
+const TIGHT_LEAGUES = new Set([61,94,197,135,140,39,2,3,848, 128]); 
+const GOLD_LEAGUES = new Set([78,262,88,253, 71, 113, 103]); 
 const MY_LEAGUES_IDS = [39,78,88,144,140,135,197,253, 71, 128];
 
 const LEAGUE_AVG_GOALS = {
@@ -31,6 +33,7 @@ const LEAGUE_AVG_GOALS = {
   135:2.48, 140:2.52, 61:2.45, 197:2.40, 94:2.45, 128: 2.30
 };
 
+// Global App State Variables
 const API_BASE = "https://v3.football.api-sports.io";
 let API_KEY = "956cbd05f9e9bf934df78d9b72d9a3a0";
 const LS_PREDS = "omega_preds_v5.0";
@@ -57,3 +60,6 @@ const SETTINGS_MAP = {
   cfg_tXG_O25:'tXG_O25',   cfg_tXG_O35:'tXG_O35',     cfg_tXG_U25:'tXG_U25',  cfg_tBTTS_U25:'tBTTS_U25',
   cfg_xG_Diff:'xG_Diff',   cfg_tBTTS:'tBTTS', cfg_minCorners:'minCorners', cfg_minCards:'minCards'
 };
+
+const _apiQueue=[]; let _apiActiveCount=0; const MAX_CONCURRENT=8; const REQUEST_GAP_MS=260;
+let _errTimer=null, _okTimer=null;
